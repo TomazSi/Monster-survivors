1862462aaa896291080610e0f71fd505ff86171c
TomazSi <146816657+TomazSi@users.noreply.github.com>
2024-04-20 11:10:50 +0200

    Hopefixed

---
+++ Assets/Enemy scripts/EnemyAttributes.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttributes : MonoBehaviour
+{
+    public int maxHealth;
+    public int health;
+    public int speed;
+    public int damage = 1;
+    public float attackSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void ChangeHealth(int change)
+    {
+        health -= change;
+        Debug.Log("Monster took " + change + " damage "+ health);
+        if (health <= 0)
+            Die();
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+        Debug.Log("Monster died");
+    }
+    public void SetStats(int maxHealth, int speed, int damage)
+    {
+        this.maxHealth = maxHealth;
+        this.speed = speed;
+        this.damage = damage;
+        if(this.health > maxHealth)
+        {
+            this.health = maxHealth;
+        }
+    }
+}
